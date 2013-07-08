0f254bfc8f8b19521a1313a3a96b9ca9d9888e95
David Karapetyan <david.karapetyan@gmail.com>
2013-07-08 13:27:02 -0400

    Added delete_edge function. Working on free_graph

--- graph.c
+++ graph.c
@@ -17,16 +17,25 @@ typedef struct graph {
 graph *create_graph(int v);
 void add_vertex(graph *MyGraph);
 void add_edge(graph *MyGraph, int src, int dest);
+void delete_edge(graph *MyGraph, int src, int dest);
+
 void print_graph(graph *MyGraph);
 void print_list(graph *MyGraph, int num);
+void free_list(graph *MyGraph, int num);
+void free_graph(graph *MyGraph);
+
+
 int main()
 
 {
     graph *MyGraph=create_graph(5);
-    add_edge(MyGraph, 3, 4);
-    add_vertex(MyGraph);
-    add_edge(MyGraph, 3, 5);
-    print_graph(MyGraph);
+    /*add_edge(MyGraph, 3, 4);*/
+    /*add_vertex(MyGraph);*/
+    /*add_edge(MyGraph, 3, 5);*/
+    /*delete_edge(MyGraph, 3, 5);*/
+    /*delete_edge(MyGraph, 3, 4);*/
+    /*print_graph(MyGraph);*/
+    free_graph(MyGraph);
     return 0;
 }
 
@@ -86,6 +95,41 @@ void add_edge(graph *MyGraph, int src, int dest) {
     }
 }
 
+void delete_edge(graph *MyGraph, int src, int dest) {
+    if (MyGraph->AdjArray[src] == NULL || MyGraph->AdjArray[dest] == NULL ) {
+        printf("%s\n", "One or both vertices don't exist");
+    }
+    else { // go along linked list, and delete a node
+        node *p, *c;  
+        for (c = MyGraph->AdjArray[src]; c; p=c, c=c->next) {
+            if (c && c->num == dest ) {
+                p->next = c->next;
+                free(c);
+                break;
+            }
+            else if (!c) {
+                printf("%s\n", "You are trying to delete an edge that doesn't exist. Enter in another edge.");
+                break;
+            }
+
+        }
+        // by symmetry, repeat above code
+        for (c = MyGraph->AdjArray[dest]; c; p=c, c=c->next) {
+            if (c && c->num == src ) {
+                p->next = c->next;
+                free(c);
+                break;
+            }
+            else if (!c) {
+                printf("%s\n", "You are trying to delete an edge that doesn't exist. Enter in another edge.");
+                break;
+            }
+
+        }
+    }
+}
+
+
 
 void print_list(graph *MyGraph, int num) {
     if (num >= MyGraph->TotalVertices || !MyGraph) 
@@ -115,3 +159,19 @@ void print_graph(graph *MyGraph) {
     else
         printf("%s\n", "Graph does not exist");
 }
+
+
+void free_list(graph *MyGraph, int num) {
+        node *c;  
+        for (c = MyGraph->AdjArray[num]; c; c=c->next) {
+            free(c);
+        }
+}
+
+void free_graph(graph *MyGraph) {
+for (int i = 0; i < MyGraph->TotalVertices; ++i) {
+free_list(MyGraph, i);
+}
+free(MyGraph);
+}
+
