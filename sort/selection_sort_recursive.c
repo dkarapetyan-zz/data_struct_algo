8e0b96d2e02f1fea486579a1c024e02a32bd31e5
David Karapetyan <david.karapetyan@gmail.com>
2013-07-22 13:38:46 -0400

    Recursive selection sort

---
+++ sort/selection_sort_recursive.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// iterative selection sort
+
+void swap(int *array, int x, int y); 
+int min_index(int *array, int start, int end);
+void selection_sort(int *array, int start, int end); 
+
+
+int main()
+{
+    int my_array[6] = {20, 17, 1, 5, 4, 3};
+    int start = 0;
+    int end = 5;
+    selection_sort(my_array, start, end);
+    for (int i=start ; i <=end ;i++ ) {
+        printf("%d ", my_array[i]);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+void selection_sort(int *array, int start, int end) {
+    if (start == end) {
+        return;
+    }
+    else if (start > end)
+    {
+        printf("%s\n", "Array start value is greater than its end value. Try again");
+    }
+    else {
+        swap(array, start, min_index(array, start, end));
+        selection_sort(array, start + 1, end);
+    }
+
+}
+
+
+
+
+
+
+int min_index(int *array, int start, int end) {
+
+    if (start > end)
+        return -1;
+    else
+    {
+
+
+
+        int pos = start;
+
+        for (int i = start; i <= end; i++) {
+            if (array[i] < array[pos]) 
+                pos = i;
+        }
+        return pos;
+    }
+}
+
+void swap(int *array, int x, int y) {
+
+    int temp = array[x];
+    array[x] = array[y];
+    array[y] = temp;
+
+}
