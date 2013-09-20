8f3edfc272435c8997a8662005f54a926606555e
David Karapetyan <david.karapetyan@gmail.com>
2013-09-20 13:43:17 -0400

    refactored

--- sort/quicksort.cpp
+++ sort/quicksort.cpp
@@ -15,7 +15,7 @@ T partition(vector<T> &my_vec, int left, int right);
 int main()
 {
     vector<double> my_vec = {1, 7.5, 4.3, 12, 9, 2};
-    quicksort<double>(my_vec, 0, my_vec.size() -1);
+    quicksort(my_vec, 0, my_vec.size() -1);
     for (auto i = my_vec.cbegin() ; i != my_vec.cend(); i++)
     {
         std::cout << *i << " ";
@@ -43,16 +43,16 @@ T partition(vector<T> &my_vec, int left, int right)
 
     // my_vec of length greater than 1
     //inductive (i.e. recursive) step
-    swap<T>(my_vec, left, pivot); // make partition element first element of my_vec
+    swap(my_vec, left, pivot); // make partition element first element of my_vec
     last = left;
     for (int i=left +1 ; i <= right ; ++i) {
         if (my_vec[i] < my_vec[left]) 
-            swap<T>(my_vec, i, ++last);
+            swap(my_vec, i, ++last);
     }
     // have all elements less than my_vec[left] immediately following
     // it--once this list terminates, have all ellements greater than my_vec[left].
     // Now we restore partition element
-    swap<T>(my_vec, left, last); //done partitioning--last is our partition element
+    swap(my_vec, left, last); //done partitioning--last is our partition element
     return last;
 }
 
@@ -69,9 +69,9 @@ void quicksort(vector<T> &my_vec, int left, int right)
     return;
 
     else {
-        T last = partition<T>(my_vec, left, right);
-        quicksort<T>(my_vec, left, last-1);
-        quicksort<T>(my_vec, last+1, right);
+        T last = partition(my_vec, left, right);
+        quicksort(my_vec, left, last-1);
+        quicksort(my_vec, last+1, right);
     }
 }
 
