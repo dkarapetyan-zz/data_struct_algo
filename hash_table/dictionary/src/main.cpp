6c124e7fd0b0b974bdaf09116d3c4e975a7f6dd3
David Karapetyan <david.karapetyan@gmail.com>
2014-12-14 23:28:21 -0500

    added src and build dir

--- hash_table/dictionary/src/main.cpp
+++ hash_table/dictionary/src/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+
 	dictionary my_table;
 	my_table.insert_entry(std::make_pair("duck", "an animal that quacks"));
 	my_table.insert_entry(std::make_pair("dubl", "collision with duck"));
@@ -9,6 +10,7 @@ int main()
 	my_table.delete_entry("duck");
 	my_table.search_entry("dubl");
 	my_table.print_entries();
+    
 
 	return 0;
 }
